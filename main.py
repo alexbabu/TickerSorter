4cef09235f413e1de11237a8485be70fd264b07b
alexbabu <alexbabu.mec@gmail.com>
2021-01-03 16:16:25 -0500

    removed sheetname from the FileHandler Class constructor arguments
    Removed Sheetname from the file Handler Class constructor.

--- main.py
+++ main.py
@@ -7,8 +7,8 @@ nMaxRecords = 100
 nMinPercent = 200
 nProgSkpVal = 100
 
+objFH = FH.cls_FileHandler('Recommendations.xlsx', nMaxRecords)
 objPM = PM.cls_ProgressManager(nProgSkpVal)
-objFH = FH.cls_FileHandler('Recommendations.xlsx', 'October', nMaxRecords)
 objRK = RK.cls_RecordKeeper(nMaxRecords, nMinPercent)
 objTP = TP.cls_ThreadPool(objPM, objRK, objFH)
 
