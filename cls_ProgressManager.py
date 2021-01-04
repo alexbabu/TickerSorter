ab46d7afc83481fd3f1d7c74273c19677dbfd28a
alexbabu <alexbabu.mec@gmail.com>
2021-01-03 21:09:57 -0500

    Update cls_ProgressManager.py
    If no data is remaining, feature added to clear the Recommendations' currrent sheet
    Removing a reduntant function call

--- cls_ProgressManager.py
+++ cls_ProgressManager.py
@@ -3,8 +3,9 @@ import json
 
 
 class cls_ProgressManager:
-    def __init__(self, nProgSkpVal):
+    def __init__(self, nProgSkpVal, objFH):
         # self.strFileName = 'Progress.txt'
+        self.__m_objFH__ = objFH
         self.m_nSkipVal = nProgSkpVal
         self.m_lst_RemainingData = []
         self.__GetRemainingDataEntries__()
@@ -37,8 +38,8 @@ class cls_ProgressManager:
             lst_strSymbolCP = strData.split(' ')
             self.m_lst_RemainingData.append([lst_strSymbolCP[0], float(lst_strSymbolCP[1])])
         if len(self.m_lst_RemainingData) == 0:
+            self.__m_objFH__.__ClearRecords__();
             self.__GetTickerandCP__()
-            self.__GetDataEntries__()
             self.__FormDataEntriesToProcess__()
         else:
             self.__GetDataEntries__()
